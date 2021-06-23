f8edd7a9234a19a2826422516a773d7220a3e264
Jonas Ampferl <jonas@ampferl.com>
2021-06-23 23:21:07 +0200

    Blockchain: Implemented simple block

---
+++ block.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
+
+func NewBlock(data string, PrevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
