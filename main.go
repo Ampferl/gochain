05f974b12d9fafc37c7e0c7cf5a3882d500bb254
Jonas Ampferl <jonas@ampferl.com>
2021-06-24 09:47:19 +0200

    CLI: Added simple CLI for the gochain

--- main.go
+++ main.go
@@ -1,24 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockchain()
+	defer bc.db.Close()
 
-	bc.AddBlock("Send 3 Coins to Jonas")
-	bc.AddBlock("Send 6 more Coins to Jonas")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
