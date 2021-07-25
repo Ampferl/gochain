fd79d88f2342dbaa09524467e4f4a10ae951bce3
Ali Chraghi <63465728+AliChraghi@users.noreply.github.com>
2021-07-25 14:09:20 +0430

    improve usage messages

--- cli.go
+++ cli.go
@@ -11,15 +11,15 @@ import (
 type CLI struct{}
 
 func (cli *CLI) printUsage() {
-	fmt.Println("Usage:")
-	fmt.Println("  createblockchain -address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
-	fmt.Println("  createwallet - Generates a new key-pair and saves it into the wallet file")
-	fmt.Println("  getbalance -address ADDRESS - Get balance of ADDRESS")
-	fmt.Println("  listaddresses - Lists all addresses from the wallet file")
-	fmt.Println("  printchain - Print all the blocks of the blockchain")
-	fmt.Println("  reindexutxo - Rebuilds the UTXO set")
-	fmt.Println("  send -from FROM -to TO -amount AMOUNT - Send AMOUNT of coins from FROM address to TO")
-	fmt.Println("  startnode -miner ADDRESS - Start a node with ID specified in NODE_ID env. var. -mine enables mining")
+	fmt.Println("- Usage")
+	fmt.Println("   createblockchain -address ADDRESS - Create a blockchain and send genesis block reward to ADDRESS")
+	fmt.Println("   createwallet - Generates a new key-pair and saves it into the wallet file")
+	fmt.Println("   getbalance -address ADDRESS - Get balance of ADDRESS")
+	fmt.Println("   listaddresses - Lists all addresses from the wallet file")
+	fmt.Println("   printchain - Print all the blocks of the blockchain")
+	fmt.Println("   reindexutxo - Rebuilds the UTXO set")
+	fmt.Println("   send -from FROM -to TO -amount AMOUNT - Send AMOUNT of coins from FROM address to TO")
+	fmt.Println("   startnode -miner ADDRESS - Start a node with ID specified in NODE_ID env. var. -mine enables mining")
 }
 
 func (cli *CLI) validateArgs() {
